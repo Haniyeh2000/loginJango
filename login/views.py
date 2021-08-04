96058ec9727f8dec4240fd4baf356c7c08a7efca
PC <mohamad.1386.yazdian@gmail.com>
2021-08-04 13:17:43 +0430

    print data from sqlite

--- login/views.py
+++ login/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Login
 
 # Create your views here.
 def login(request):
-    return render(request,'login/index.html')
+    users = Login.objects.all()
+    return render(request,'login/index.html', {'users': users})
 
 def detail (request,question_id):
     return HttpResponse("youre looking at question %s hhhhh" % question_id)
